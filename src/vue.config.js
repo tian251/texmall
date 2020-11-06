c72a213d6efdf64a0cc19c0b32d4ff414d1841dd
tiam251 <2516839719@qq.com>
2020-11-06 23:39:12 +0800

    项目预备

---
+++ src/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    configureWebpack: {
+        resolve: {
+            alias: {
+                'assets': '@/assest',
+                'common': '@/common',
+                'components': '@/componentst',
+                'network': '@/network',
+                'assets': '@/assest',
+                'views': '@views'
+            }
+        }
+    }
+}
